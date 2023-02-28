0da0d17ba7795e1fdae4b41adbf566b2590003c9
Abby Humphreys <bibby3316@gmail.com>
2023-02-28 12:59:03 +0000

    add: functionality to retrieve form inputs, send via emailjs and display success/error messages

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,21 @@
-console.log("hello");
+function sendMail(contactForm) {
+    let templateParams = {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value,
+    };
+    emailjs.send("service_v2xkqij", "template_9398qf9", templateParams, "lOit3QM5Exl4CLX_f") //serviceID, templateId, templateParams, publicKey
+    .then (
+        function(res) {
+            console.log("SUCCESS", res);
+            const successElement = document.getElementById('success-element');
+            successElement.classList.remove('hide');
+        },
+        function(err) {
+            console.log("FAILED", err);
+            const failedElement = document.getElementById('failed-element');
+            failedElement.classList.remove('hide');
+        }
+    );
+    return false;
+}
