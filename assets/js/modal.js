e72de549a7571406c7d80fba38c37a7acd5b5e71
Abby Humphreys <bibby3316@gmail.com>
2023-03-16 22:42:03 +0000

    add: comments to modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,11 @@
+/**
+ * JQUERY used to show Modal 1 when page loads
+ * For use with game.html Modal 1
+ * */
+
+/* Placed in seperate file from game.js
+to ease potential use of jest for testing */ 
+
 $(document).ready(function () {
     $("#myModal").modal('show');
 });
